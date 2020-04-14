b992974a6ba7827e8780147bb80bb9fdc4dd73ef
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 01:52:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,13 @@
-not_safe?(55)
 
-	#  def unsafe?(speed)
-	# 	 if speed > 60
-	# 		 return true
-	# 	 elsif speed < 40
-	# 		 return true
-	# 	 else
-	# 		 return false
-	#  end
+	 def unsafe?(speed)
+		 if speed > 60
+			 return true
+		 elsif speed < 40
+			 return true
+		 else
+			 return false
+	 end
 
 	 def not_safe?(speed)
 		 (speed > 60 || speed < 40)? true : false
 	 end
-
-
-not_safe?(55)
