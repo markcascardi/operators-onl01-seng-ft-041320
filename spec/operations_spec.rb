749156dbb1a2b407da4eebcdc6717e09c7903704
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 01:12:33 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -24,22 +24,22 @@ describe "Operations" do
   #     answer = not_safe?(79)
   #     expect(answer).to eq(true)
   #   end
-  # 
+  #
   #   it 'returns true if speed is less than 40' do
   #     answer = not_safe?(35)
   #     expect(answer).to eq(true)
   #   end
-  # 
+  #
   #   it 'returns false if the speed is between 40 and 60' do
   #     answer = not_safe?(50)
   #     expect(answer).to eq(false)
   #   end
-  # 
+  #
   #   it 'uses the ternary operator' do
   #     methods = file_contents.split("not_safe?")
   #     match = methods.last
   #     expect(match).to include("?")
   #   end
   # end
-  
+
 end
