193873a16b8d3fc0d699607853a744481650a038
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 01:28:31 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -2,22 +2,22 @@
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
 
-  describe "#unsafe?" do
-    it 'returns true if speed is greater than 60' do
-      answer = unsafe?(79)
-      expect(answer).to eq(true)
-    end
-
-    it 'returns true if speed is less than 40' do
-      answer = unsafe?(35)
-      expect(answer).to eq(true)
-    end
-
-    it 'returns false if the speed is between 40 and 60' do
-      answer = unsafe?(50)
-      expect(answer).to eq(false)
-    end
-  end
+  # describe "#unsafe?" do
+  #   it 'returns true if speed is greater than 60' do
+  #     answer = unsafe?(79)
+  #     expect(answer).to eq(true)
+  #   end
+  # 
+  #   it 'returns true if speed is less than 40' do
+  #     answer = unsafe?(35)
+  #     expect(answer).to eq(true)
+  #   end
+  # 
+  #   it 'returns false if the speed is between 40 and 60' do
+  #     answer = unsafe?(50)
+  #     expect(answer).to eq(false)
+  #   end
+  # end
 
   describe "#not_safe?" do
     it 'returns true if speed is greater than 60' do
